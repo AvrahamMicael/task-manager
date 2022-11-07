a53af1323411e14ef97c89bce7956bdba5b7fd1e
Avraham Micael <avrahammicael@protonmail.com>
2022-11-07 11:20:40 -0300

    comments removed

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -7,29 +7,11 @@ module.exports.getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   return res.json(tasks);
 });
-// try
-// {
-//   const tasks = await Task.find({});
-//   return res.json(tasks);
-// }
-// catch(error)
-// {
-//   return res.status(500).json(error);
-// }
 
 module.exports.createTask = asyncWrapper(async ({ body }, res) => {
   const task = await Task.create(body);
   return res.status(201).json(task);
 });
-// try
-// {
-//   const task = await Task.create(body);
-//   return res.status(201).json(task);
-// }
-// catch(error)
-// {
-//   return res.status(500).json(error);
-// }
 
 module.exports.getTask = asyncWrapper(async ({ params: { id } }, res, next) => {
   if(!isValidObjectId(id)) throw 'Id is not valid';
@@ -37,17 +19,6 @@ module.exports.getTask = asyncWrapper(async ({ params: { id } }, res, next) => {
   if(!task) return next(new ErrorWithStatus('Not found', 404));
   return res.json(task);
 });
-// try
-// {
-//   if(!isValidObjectId(id)) throw 'Id is not valid';
-//   const task = await Task.findById(id);
-//   if(!task) return res.status(404).json({ msg: `No task with id: ${id}` });
-//   return res.json(task);
-// }
-// catch(error)
-// {
-//   return res.status(500).json({ msg: error });
-// }
 
 module.exports.updateTask = asyncWrapper(async ({ params: { id }, body }, res) => {
   if(!isValidObjectId(id)) throw 'Id is not valid';
@@ -55,17 +26,6 @@ module.exports.updateTask = asyncWrapper(async ({ params: { id }, body }, res) =
   if(!task) return res.status(404).json({ msg: `No task with id: ${id}` });
   return res.status(200).json(task);
 });
-// try
-// {
-//   if(!isValidObjectId(id)) throw 'Id is not valid';
-//   const task = await Task.findByIdAndUpdate(id, body, { new: true, runValidators: true });
-//   if(!task) return res.status(404).json({ msg: `No task with id: ${id}` });
-//   return res.status(200).json(task);
-// }
-// catch(error)
-// {
-//   return res.status(500).json({ msg: error });
-// }
 
 module.exports.deleteTask = asyncWrapper(async ({ params: { id } }, res) => {
   if(!isValidObjectId(id)) throw 'Id is not valid';
@@ -73,14 +33,3 @@ module.exports.deleteTask = asyncWrapper(async ({ params: { id } }, res) => {
   if(!task) return res.status(404).json({ msg: `No task with id: ${id}` });
   return res.status(204).send();
 });
-// try
-// {
-//   if(!isValidObjectId(id)) throw 'Id is not valid';
-//   const task = await Task.findByIdAndDelete(id);
-//   if(!task) return res.status(404).json({ msg: `No task with id: ${id}` });
-//   return res.status(204).send();
-// }
-// catch(error)
-// {
-//   return res.status(500).json({ msg: error });
-// }
